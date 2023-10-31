4bf198125596440fa1ccf5129a172eae9d3ddca5
nika <nikaonime@gmail.com>
2023-10-31 13:15:55 +0200

    Login, react-hook-form

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ import Grid from '@mui/material/Grid';
 import { Post } from '../components/Post';
 import { TagsBlock } from '../components/TagsBlock';
 import { CommentsBlock } from '../components/CommentsBlock';
-import { fetchPosts, fetchTags } from '../redux/slices/Posts';
+import { fetchPosts, fetchTags } from '../redux/slices/posts';
 
 export const Home = () => {
     const dispatch = useDispatch();
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,28 +1,70 @@
-import React from "react";
-import Typography from "@mui/material/Typography";
-import TextField from "@mui/material/TextField";
-import Paper from "@mui/material/Paper";
-import Button from "@mui/material/Button";
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
-import styles from "./Login.module.scss";
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+
+import styles from './Login.module.scss';
+import { fetchAuth } from '../../redux/slices/auth';
 
 export const Login = () => {
-  return (
-    <Paper classes={{ root: styles.root }}>
-      <Typography classes={{ root: styles.title }} variant="h5">
-        Вход в аккаунт
-      </Typography>
-      <TextField
-        className={styles.field}
-        label="E-Mail"
-        error
-        helperText="Неверно указана почта"
-        fullWidth
-      />
-      <TextField className={styles.field} label="Пароль" fullWidth />
-      <Button size="large" variant="contained" fullWidth>
-        Войти
-      </Button>
-    </Paper>
-  );
+    const dispatch = useDispatch();
+
+    // react-hook-form
+    const {
+        register,
+        handleSubmit,
+        setError,
+        formState: { errors, isValid },
+    } = useForm({
+        defaultValues: {
+            email: '',
+            password: '',
+        },
+        mode: 'onChange',
+    });
+
+    const onSubmit = (values) => {
+        dispatch(fetchAuth(values));
+    };
+
+    // validation
+    return (
+        <Paper classes={{ root: styles.root }}>
+            <Typography classes={{ root: styles.title }} variant="h5">
+                Вход в аккаунт
+            </Typography>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <TextField
+                    className={styles.field}
+                    label="E-Mail"
+                    type="email"
+                    fullWidth
+                    error={Boolean(errors.email?.message)}
+                    helperText={errors.email?.message}
+                    {...register('email', { required: 'Enter email' })}
+                />
+                <TextField
+                    className={styles.field}
+                    label="Пароль"
+                    fullWidth
+                    helperText={errors.password?.message}
+                    error={Boolean(errors.password?.message)}
+                    {...register('password', { required: 'Enter password' })}
+                />
+                <Button
+                    type="submit"
+                    variant="contained"
+                    size="large"
+                    fullWidth
+                >
+                    Войти
+                </Button>
+            </form>
+        </Paper>
+    );
 };
